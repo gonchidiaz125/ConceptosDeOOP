d5c3d7b4f77824eb47ea5d91199f9a38bb5aaf37
Gonzalo Andres Diaz <gonchi.diaz125@gmail.com>
2024-07-30 12:58:31 -0300

    Muevo interfaces y clases de operaciones matematicas a otro archivo

---
+++ ConceptosDeOOP/EjemplosDeInterfaces/OperacionesMatematicas/OperacionesMatematicas.cs
@@ -0,0 +1,61 @@
+﻿namespace ConceptosDeOOP.EjemplosDeInterfaces.OperacionesMatematicas
+{
+    /// <summary>
+    /// La interfaz IOperacionMatematica define los métodos comunes que debe implementar (tener) cualquier clase que represente una operacion matematica (con clase me refiero a clase de orientacion objetos) 
+    /// </summary>
+    public interface IOperacionMatematica
+    {
+        public double Calcular(double numero1, double numero2);
+
+        public string ObtenerMensajeConResultado(double numero1, double numero2);
+
+    }
+
+    public class CalculadoraDeSuma : IOperacionMatematica
+    {
+        private readonly string _operacion = "Suma";
+
+        public double Calcular(double numero1, double numero2)
+        {
+            return numero1 + numero2;
+        }
+
+        public string ObtenerMensajeConResultado(double numero1, double numero2)
+        {
+            var resultado = Calcular(numero1, numero2);
+            return $"La {_operacion} de {numero1} + {numero2} es {resultado}";
+        }
+    }
+
+    public class CalculadoraDeResta : IOperacionMatematica
+    {
+        private readonly string _operacion = "Resta";
+
+        public double Calcular(double numero1, double numero2)
+        {
+            return numero1 - numero2;
+        }
+
+        public string ObtenerMensajeConResultado(double numero1, double numero2)
+        {
+            var resultado = Calcular(numero1, numero2);
+            return $"La {_operacion} de {numero1} - {numero2} es {resultado}";
+        }
+    }
+
+    public class CalculadoraDeMultiplicacion : IOperacionMatematica
+    {
+        private readonly string _operacion = "Multiplicación";
+
+        public double Calcular(double numero1, double numero2)
+        {
+            return numero1 * numero2;
+        }
+
+        public string ObtenerMensajeConResultado(double numero1, double numero2)
+        {
+            var resultado = Calcular(numero1, numero2);
+            return $"La {_operacion} de {numero1} * {numero2} es {resultado}";
+        }
+    }
+}
--- ConceptosDeOOP/EjemplosDeInterfaces/program.cs
+++ ConceptosDeOOP/EjemplosDeInterfaces/program.cs
@@ -1,65 +1,67 @@
 ﻿using ConceptosDeOOP.EjemplosDeInterfaces.Logger;
+using ConceptosDeOOP.EjemplosDeInterfaces.OperacionesMatematicas;
 
 Console.WriteLine("Hola. Este son ejemplo de OOP (Object Oriented Programing). Se incluye el uso de Interfaces, clases y herencia");
 Console.WriteLine("");
 
-var num1 = 20.5d;
-var num2 = 10.2d;
 
-/* Primer ejemplo: Voy a crear un objeto de cada tipo de operación y lo utilizo para hacer el cálculo
-   Como se puede ver el código es muy similar en cada caso, el principal cambio es que se utiliza una calculadora diferente en cada caso
- */
-
-Console.WriteLine("Ejemplo 1");
-Console.WriteLine("");
-
-var sumador = new CalculadoraDeSuma();
-var resultado = sumador.Calcular(num1, num2);
-Console.WriteLine($"Resultado ({num1} + {num2}): {resultado}");
-Console.WriteLine(sumador.ObtenerMensajeConResultado(num1, num2));
-Console.WriteLine("");
-
-var restador = new CalculadoraDeResta();
-resultado = restador.Calcular(num1, num2);
-Console.WriteLine($"Resultado ({num1} - {num2}): {resultado}");
-Console.WriteLine(restador.ObtenerMensajeConResultado(num1, num2));
-Console.WriteLine("");
-
-var multiplicador = new CalculadoraDeMultiplicacion();
-resultado = multiplicador.Calcular(num1, num2);
-Console.WriteLine($"Resultado ({num1} * {num2}): {resultado}");
-Console.WriteLine(multiplicador.ObtenerMensajeConResultado(num1, num2));
-Console.WriteLine("");
-Console.WriteLine("");
-Console.WriteLine("");
+// Ejemplos 1 y 2 con Operaciones matematicas
+EjemploDeOperacionesMatematicas();
 
 
-/*
-   Segundo ejemplo: Voy a crear un objeto de cada tipo de calculadora, pero voy a usar un función permita ejecutar cualquier tipo de calculadora
- */
-
-Console.WriteLine("Ejemplo 2");
-Console.WriteLine("");
-
-var sumadorEjemplo2 = new CalculadoraDeSuma();
-var restaEjemplo2 = new CalculadoraDeResta();
-var multiplicadorEjemplo2 = new CalculadoraDeMultiplicacion();
-
-EjecutarOperacion(num1, num2, sumadorEjemplo2);
-EjecutarOperacion(num1, num2, restaEjemplo2);
-EjecutarOperacion(num1, num2, multiplicadorEjemplo2);
-
-
-/*
-  Tercer ejemplo: Usar una interfaz ILogger para dar soporte a diferentes tipos de loggers (a consola, a base de datos, a un servidor en la nube que guarde los logs)
- 
- */
-Console.WriteLine("Ejemplo 3");
-Console.WriteLine("");
-
+// Ejemplo 3 de los Logger
 EjemploDeUsoDeLogger();
 
 
+
+void EjemploDeOperacionesMatematicas()
+{
+    var num1 = 20.5d;
+    var num2 = 10.2d;
+
+    /* Primer ejemplo: Voy a crear un objeto de cada tipo de operación y lo utilizo para hacer el cálculo
+       Como se puede ver el código es muy similar en cada caso, el principal cambio es que se utiliza una calculadora diferente en cada caso
+     */
+
+    Console.WriteLine("Ejemplo 1");
+    Console.WriteLine("");
+
+    var sumador = new CalculadoraDeSuma();
+    var resultado = sumador.Calcular(num1, num2);
+    Console.WriteLine($"Resultado ({num1} + {num2}): {resultado}");
+    Console.WriteLine(sumador.ObtenerMensajeConResultado(num1, num2));
+    Console.WriteLine("");
+
+    var restador = new CalculadoraDeResta();
+    resultado = restador.Calcular(num1, num2);
+    Console.WriteLine($"Resultado ({num1} - {num2}): {resultado}");
+    Console.WriteLine(restador.ObtenerMensajeConResultado(num1, num2));
+    Console.WriteLine("");
+
+    var multiplicador = new CalculadoraDeMultiplicacion();
+    resultado = multiplicador.Calcular(num1, num2);
+    Console.WriteLine($"Resultado ({num1} * {num2}): {resultado}");
+    Console.WriteLine(multiplicador.ObtenerMensajeConResultado(num1, num2));
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+
+
+    /*
+       Segundo ejemplo: Voy a crear un objeto de cada tipo de calculadora, pero voy a usar un función permita ejecutar cualquier tipo de calculadora
+     */
+
+    Console.WriteLine("Ejemplo 2");
+    Console.WriteLine("");
+
+    var sumadorEjemplo2 = new CalculadoraDeSuma();
+    var restaEjemplo2 = new CalculadoraDeResta();
+    var multiplicadorEjemplo2 = new CalculadoraDeMultiplicacion();
+
+    EjecutarOperacion(num1, num2, sumadorEjemplo2);
+    EjecutarOperacion(num1, num2, restaEjemplo2);
+    EjecutarOperacion(num1, num2, multiplicadorEjemplo2);
+}
 
 
 double EjecutarOperacion(double num1, double num2, IOperacionMatematica operacion)
@@ -71,7 +73,11 @@ double EjecutarOperacion(double num1, double num2, IOperacionMatematica operacio
 }
 
 void EjemploDeUsoDeLogger()
-{
+{    
+    Console.WriteLine("Ejemplo 3");
+    Console.WriteLine("Usar una interfaz ILogger para dar soporte a diferentes tipos de loggers (a consola, a base de datos, a un servidor en la nube que guarde los logs) ");
+    Console.WriteLine("");
+
     var logearEnConsola = new ConsoleLogger();
     var gestorCuentaBancaria = new GestorDeCuentaBanciaria(logearEnConsola);
 
@@ -86,7 +92,7 @@ void EjemploDeUsoDeLogger()
 
     // Caso 2: Depósito fallido debido a monto = 0 (ConsoleLogger)
     Console.WriteLine("");
-    Console.WriteLine("Caso 2: Deposito exitoso (usando ConsoleLogger)");
+    Console.WriteLine("Caso 2: Deposito FALLIDO (usando ConsoleLogger)");
     Console.WriteLine("");
     monto = 0d;
     gestorCuentaBancaria.ProcesarDepositoBancario(idCliente, numeroDeCuenta, monto);
@@ -105,71 +111,14 @@ void EjemploDeUsoDeLogger()
 
     // Caso 4: Depósito fallido debido a monto = 0 (ConsoleLogger)
     Console.WriteLine("");
-    Console.WriteLine("Caso 4: Deposito exitoso (usando DataBaseLogger)");
+    Console.WriteLine("Caso 4: Deposito FALLIDO (usando DataBaseLogger)");
     Console.WriteLine("");
     monto = 0d;
     gestorCuentaBancaria.ProcesarDepositoBancario(idCliente, numeroDeCuenta, monto);
 
 }
 
-/// <summary>
-/// La interfaz IOperacionMatematica define los métodos comunes que debe implementar (tener) cualquier clase que represente una operacion matematica (con clase me refiero a clase de orientacion objetos) 
-/// </summary>
-public interface IOperacionMatematica
-{
-    public double Calcular(double numero1, double numero2);
 
-    public string ObtenerMensajeConResultado(double numero1, double numero2);
-
-}
-
-public class CalculadoraDeSuma : IOperacionMatematica
-{
-    private readonly string _operacion = "Suma";
-
-    public double Calcular(double numero1, double numero2)
-    {
-        return numero1 + numero2;
-    }
-
-    public string ObtenerMensajeConResultado(double numero1, double numero2)
-    {
-        var resultado = Calcular(numero1, numero2);
-        return $"La {_operacion} de {numero1} + {numero2} es {resultado}";
-    }
-}
-
-public class CalculadoraDeResta : IOperacionMatematica
-{
-    private readonly string _operacion = "Resta";
-
-    public double Calcular(double numero1, double numero2)
-    {
-        return numero1 - numero2;
-    }
-
-    public string ObtenerMensajeConResultado(double numero1, double numero2)
-    {
-        var resultado = Calcular(numero1, numero2);
-        return $"La {_operacion} de {numero1} - {numero2} es {resultado}";
-    }
-}
-
-public class CalculadoraDeMultiplicacion : IOperacionMatematica
-{
-    private readonly string _operacion = "Multiplicación";
-
-    public double Calcular(double numero1, double numero2)
-    {
-        return numero1 * numero2;
-    }
-
-    public string ObtenerMensajeConResultado(double numero1, double numero2)
-    {
-        var resultado = Calcular(numero1, numero2);
-        return $"La {_operacion} de {numero1} * {numero2} es {resultado}";
-    }
-}
 
 
 
